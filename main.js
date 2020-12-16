e5accf66d462c3bb3d52752049cc1503e019df8d
Alessandro <marzucoalessandro1@gmail.com>
2020-12-16 16:28:10 +0100

    alert x newThing con meno di 5 caratteri

--- main.js
+++ main.js
@@ -3,17 +3,17 @@
 // Se non sapere che fare, di seguito trovate uno screenshot.
 // Funzionalitá:
 // La nostra todo list avrá alcune tasks di default predefinite
-// L'utente puó inserire nuove tasks
-// Cliccando sulla "X" l'utente puó cancellare una task
-// Se non ci sono piu task nella lista, mostrate un messaggio tipo "Nulla da fare"
-// Quando l'utente inserisce una task ha due modi per salvarla: o preme il pulsante add o preme il taso Enter della tastiera.
+// L'utente puó inserire nuove tasks.....fatto;
+// Cliccando sulla "X" l'utente puó cancellare una task.....fatto;
+// Se non ci sono piu task nella lista, mostrate un messaggio tipo "Nulla da fare".....fatto;
+// Quando l'utente inserisce una task ha due modi per salvarla: o preme il pulsante add o preme il taso Enter della tastiera....fatto;
 // Attenzione: l'utente non deve inserire tasks vuote ma almeno un tot di caratteri.
 
 let app = new Vue ({
   el: "#root",
   data: {
     newThing: "",
-
+    message: "devi inserire qlk comprensibile",
     myList: [
       "do something",
       "do else",
@@ -22,7 +22,12 @@ let app = new Vue ({
   },
   methods: {
     addThing (){
-      this.myList.push(this.newThing);
+      if (this.newThing.length < 5) {
+        alert("inserisci qlk di comprensibile");
+      } else {
+        this.myList.push(this.newThing);
+      }
+
 
   },
   removeThing(){
