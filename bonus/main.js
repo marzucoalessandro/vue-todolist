a6ad4620144065974544e337f6eef8507673cda6
Alessandro <marzucoalessandro1@gmail.com>
2020-12-17 12:56:21 +0100

    funzione per modificare un item non funzionante

--- bonus/main.js
+++ bonus/main.js
@@ -9,16 +9,25 @@
 // Quando l'utente inserisce una task ha due modi per salvarla: o preme il pulsante add o preme il taso Enter della tastiera....fatto;
 // Attenzione: l'utente non deve inserire tasks vuote ma almeno un tot di caratteri.....fatto;
 
+
+// Istruzioni Bonus (da mettere in una cartella bonus):
+// L'utente vuole poter modificare una task giá inserita
+// ma vuole anche poter indicare che la task é stata completata
+// inoltre se una task é stata completa allora vuole che venga inserita in un'altra colonna tipo "tasks completate"
+// ah non é finita, dice che quando cancella una task, non vuole che questa venga subito rimossa, ma vuole che resti visibile ma venga spostata in una colonna tipo "cestino"
+// si, l'utente é un rompi scatole, dice infine che vuole poter rimuovere tutte le tasks nel cestino cliccando su un pulsante tipo "svuota cestino"
+
+
 let app = new Vue ({
   el: "#root",
   data: {
     newThing: "",
-
     myList: [
       "do something",
       "do else",
       "do something else"
-    ]
+    ],
+    replaceThing: ""
   },
   methods: {
     addThing (){
@@ -31,8 +40,13 @@ let app = new Vue ({
     removeThing(){
       this.myList.pop(this.myList.item)
 
+    },
+    replace(){
+      replaceThing: prompt("modifica questo item"),
+      this.myList.pop(this.myList.item),
+      this.mylist.push(this.replaceThing)
+
+
     }
   },
-
-
 })
